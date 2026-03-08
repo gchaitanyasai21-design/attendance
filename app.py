54312dc33ac573584fede0059fb205adb9260c1b
gchai <gchai@local>
2026-03-09 01:20:18 +0530

    chore: clean trailing whitespace

--- app.py
+++ app.py
@@ -1903,5 +1903,4 @@ if __name__ == "__main__":
     with app.app_context():
         init_db()
         normalize_attendance_window_if_needed()
-         
     app.run(host="0.0.0.0", port=5000, debug=True)
