19173a4f44db9662202891af5a700d2053917e54
gchai <gchai@local>
2026-03-09 00:37:02 +0530

    test: trigger render auto deploy

--- app.py
+++ app.py
@@ -1902,4 +1902,5 @@ if __name__ == "__main__":
     with app.app_context():
         init_db()
         normalize_attendance_window_if_needed()
+         
     app.run(host="0.0.0.0", port=5000, debug=True)
